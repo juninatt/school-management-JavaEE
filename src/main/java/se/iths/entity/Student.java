8ce98e28a247b124449042fd71a5751722cf02a9
PetterBT <petter.bergstrom@iths.se>
2022-02-24 10:29:31 +0100

    Add annotations to Student.java

--- src/main/java/se/iths/entity/Student.java
+++ src/main/java/se/iths/entity/Student.java
@@ -1,12 +1,27 @@
 package se.iths.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
 
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotEmpty
     private String firstName;
+
+    @NotEmpty
     private String lastName;
+
+    @NotEmpty
     private String email;
+
     private String phoneNumber;
 
 }
