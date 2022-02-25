7f4bc6fb22ce974478aa312244260cecfb06db8a
PetterBT <petter.bergstrom@iths.se>
2022-02-25 12:54:31 +0100

    Add createStudent-methods

--- src/main/java/se/iths/rest/StudentControl.java
+++ src/main/java/se/iths/rest/StudentControl.java
@@ -1,7 +1,29 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentControl {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("new")
+    @POST
+    public Response createStudent(Student student) {
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
 }
+
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,19 @@
 package se.iths.service;
 
+import se.iths.entity.Student;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
 public class StudentService {
 
+    @PersistenceContext
+    EntityManager entityManager;
 
+    public Student createStudent(Student student) {
+        entityManager.persist(student);
+        return  student;
+    }
 }
