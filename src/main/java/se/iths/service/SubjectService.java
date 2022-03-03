3deed2539931ec3961ea6413c93dd1324beb3e3c
PetterBT <petter.bergstrom@iths.se>
2022-03-03 10:58:16 +0100

    Add SubjectService.java and TeacherService.java

---
+++ src/main/java/se/iths/service/SubjectService.java
@@ -0,0 +1,19 @@
+package se.iths.service;
+
+import se.iths.entity.Subject;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Transactional
+public class SubjectService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Subject createSubject(Subject subject) {
+        entityManager.persist(subject);
+        return subject;
+    }
+}
