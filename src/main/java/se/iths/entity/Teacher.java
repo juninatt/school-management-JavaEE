5a42cbae690208d2995e8cf0d44729abe781b8bb
PetterBT <petter.bergstrom@iths.se>
2022-03-04 12:09:03 +0100

    Add setId-method to Subject.java

--- src/main/java/se/iths/entity/Teacher.java
+++ src/main/java/se/iths/entity/Teacher.java
@@ -28,6 +28,9 @@ public class Teacher {
     public long getId() {
         return id;
     }
+    public void setId(long id) {
+        this.id = id;
+    }
     public String getFirstName() {
         return firstName;
     }
