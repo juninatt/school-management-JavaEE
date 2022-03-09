31dc0728fd57f2e3b681f44cea5572dec9f1cee0
PetterBT <petter.bergstrom@iths.se>
2022-03-09 11:04:40 +0100

    Make getSubjects-method in Teacher class JsonbTransient, instead of getTeacher-method in Subject class

--- src/main/java/se/iths/entity/Subject.java
+++ src/main/java/se/iths/entity/Subject.java
@@ -28,7 +28,6 @@ public class Subject {
     }
     public Subject() {}
 
-    @JsonbTransient
     public Teacher getTeacher() {
         return teacher;
     }
--- src/main/java/se/iths/entity/Teacher.java
+++ src/main/java/se/iths/entity/Teacher.java
@@ -57,7 +57,7 @@ public class Teacher {
         this.phoneNumber = phoneNumber;
     }
 
-
+    @JsonbTransient
     public List<Subject> getSubjects() {
         return subjects;
     }
